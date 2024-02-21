7d67950516fc4f0e563067b4afd70ed9cb7ab509
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-02-21 05:28:53 -0800

    Fix Matrix in Problem_3

--- JS Basics/Loops/Problem_3/index.js
+++ JS Basics/Loops/Problem_3/index.js
@@ -1,15 +1,26 @@
-let result;
+function getNumberToAdd(x, y) {
+    return x === y ? '1' : '0'
+}
 
 function matrixOfNumbers(n) {
+    let result = '';
     if(n > 1 && n < 20) {
-        let matrix = [];
-        for(let i = 0; i < n; i++) {
-            matrix[i] = [];
-            for(let j = 0; j < n; j++) {
-                matrix[i][j] = 0;
+        debugger;
+        let rows = [];
+        for (let j = 0; j < n; j++) {
+            let row = ''
+            for(let i = 0; i < n; i++) {
+                let numberToAdd = getNumberToAdd(i, j);
+                row += ` ${numberToAdd}`;
             }
-            result = matrix.map((row) => row.join(" ")).join("\n");
+            rows.push(row);
         }
+
+        for (let k = 0; k < rows.length; k++) {
+            const delimeter = k == 0 && k == rows.length - 1 ? '' : '\n';
+            result += `${delimeter}${rows[k]}`
+        }
+
         
     } else {
         result = "Invalid input";
@@ -17,4 +28,113 @@ function matrixOfNumbers(n) {
     return result;
 }
 
-matrixOfNumbers(5);
+console.log(matrixOfNumbers(5));
+
+// reversed diagonal
+/*
+function getNumberToAdd(x, y, n) {
+    return y === n - x - 1 ? '1' : '0'
+}
+
+function matrixOfNumbers(n) {
+    let result = '';
+    if(n > 1 && n < 20) {
+        let rows = [];
+        for (let j = 0; j < n; j++) {
+            let row = '';
+            for(let i = 0; i < n; i++) {
+                let numberToAdd = getNumberToAdd(i, j, n);
+                row += ` ${numberToAdd}`;
+            }
+            rows.push(row);
+        }
+
+        for (let k = 0; k < rows.length; k++) {
+            const delimeter = k ==0 && k == rows.length - 1 ? '' : '\n';
+            result += `${delimeter}${rows[k]}`
+        }
+
+        
+    } else {
+        result = "Invalid input";
+    }
+    return result;
+}
+
+console.log(matrixOfNumbers(10));
+
+*/
+
+// both diagonals
+/*
+function getNumberToAdd(x, y, n) {
+    return (y === n - x - 1) || x === y ? '1' : '0'
+}
+
+function matrixOfNumbers(n) {
+    let result = '';
+    if(n > 1 && n < 20) {
+        let rows = [];
+        for (let j = 0; j < n; j++) {
+            let row = '';
+            for(let i = 0; i < n; i++) {
+                let numberToAdd = getNumberToAdd(i, j, n);
+                row += ` ${numberToAdd}`;
+            }
+            rows.push(row);
+        }
+
+        for (let k = 0; k < rows.length; k++) {
+            const delimeter = k ==0 && k == rows.length - 1 ? '' : '\n';
+            result += `${delimeter}${rows[k]}`
+        }
+
+        
+    } else {
+        result = "Invalid input";
+    }
+    return result;
+}
+
+console.log(matrixOfNumbers(10));
+
+*/
+
+// print ones only on targeted row/col
+/*
+function getNumberToAdd(x, y, target) {
+    return x === target || y === target ? "1" : "0"
+}
+
+function matrixOfNumbers(n) {
+    let result = '';
+    let targetRowCol = parseInt(prompt());
+    if(targetRowCol > n - 1) {
+        return "Invalid"
+    }
+    if(n > 1 && n < 20) {
+        //debugger;
+        let rows = [];
+        for (let j = 0; j < n; j++) {
+            let row = '';
+            for(let i = 0; i < n; i++) {
+                let numberToAdd = getNumberToAdd(i, j, targetRowCol);
+                row += ` ${numberToAdd}`;
+            }
+            rows.push(row);
+        }
+
+        for (let k = 0; k < rows.length; k++) {
+            const delimeter = k == 0 && k == rows.length - 1 ? '' : '\n';
+            result += `${delimeter}${rows[k]}`
+        }
+
+        
+    } else {
+        result = "Invalid input";
+    }
+    return result;
+}
+
+console.log(matrixOfNumbers(9));
+*/
