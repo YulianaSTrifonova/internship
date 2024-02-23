ecfd11f416caddb6683ed2f18dc196df20e17e28
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-02-23 06:44:17 -0800

    Fix Problem 1

--- JS Basics/Arrays/Problem_1/index.js
+++ JS Basics/Arrays/Problem_1/index.js
@@ -1,10 +1,7 @@
 const n = prompt("Enter a number");
-let randomNumber = 0;
 let integerArray = [];
 
 for(let i = 0; i < n; i++) {
-    randomNumber = parseInt(Math.random() * 10);
-    integerArray.push(randomNumber);
+    integerArray[i] = i * 5;
+    console.log(integerArray[i]);
 }
-
-integerArray.forEach((element) => console.log(element * 5));
