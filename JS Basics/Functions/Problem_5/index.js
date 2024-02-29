5bf1cd7e234ab883166d11ead037409feb96f558
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-02-29 06:41:22 -0800

    Fix Problem 5

--- JS Basics/Functions/Problem_5/index.js
+++ JS Basics/Functions/Problem_5/index.js
@@ -7,15 +7,17 @@ for(let j = 0; j < n; j++) {
     inputArr.push(num);
 }
 
+function ifLarger(arr, index) {
+    return arr[index] > arr[index - 1] && arr[index] > arr[index + 1] ? true : false;
+}
+
 function largerThanNeighbors(arr) {
     let count = 0;
-
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+        if(checkIfLarger(arr, i)){
             count++;
         }
     }
-
     return count;
 }
 
