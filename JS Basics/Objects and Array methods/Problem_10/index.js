fe874e6cb4e76b13168ab6ae7a54f3b4b67c6d0c
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-03-11 06:39:55 -0700

    Fix Problems 6, 7 and 10

--- JS Basics/Objects and Array methods/Problem_10/index.js
+++ JS Basics/Objects and Array methods/Problem_10/index.js
@@ -1,20 +1,22 @@
 function youngestMale(arr) {
-    let youngestMale = arr.find((person) => person.gender === "male")
-    arr.forEach((person) => {
-        if(person.gender === "male" && person.age < youngestMale.age) {
-            youngestMale = person;
-        }
+  let youngestMale = arr.find((person) => person.gender === "male");
+  arr
+    .filter((person) => person.gender === "male")
+    .forEach((male) => {
+      if (youngestMale.age > male.age) {
+        youngestMale = male;
+      }
     });
-    return youngestMale.firstname + " " + youngestMale.lastname;
+  return youngestMale.firstname + " " + youngestMale.lastname;
 }
 
 let people = [
-    { firstname: 'Gosho', lastname: 'Petrov', age: 16, gender: 'male' },
-    { firstname: 'Bay', lastname: 'Ivan', age: 81, gender: 'male' },
-    { firstname: 'John', lastname: 'Doe', age: 42, gender: 'female' },
-    { firstname: 'Pesho', lastname: 'Pesho', age: 15, gender: 'male' },
-    { firstname: 'Asdf', lastname: 'Xyz', age: 17, gender: 'female' },
-    { firstname: 'Gosho', lastname: 'Gosho', age: 22, gender: 'male' }
+  { firstname: "Gosho", lastname: "Petrov", age: 16, gender: "male" },
+  { firstname: "Bay", lastname: "Ivan", age: 81, gender: "male" },
+  { firstname: "John", lastname: "Doe", age: 42, gender: "female" },
+  { firstname: "Pesho", lastname: "Pesho", age: 15, gender: "male" },
+  { firstname: "Asdf", lastname: "Xyz", age: 17, gender: "female" },
+  { firstname: "Gosho", lastname: "Gosho", age: 22, gender: "male" },
 ];
 
 youngestMale(people);
--- JS Basics/Objects and Array methods/Problem_6/index.js
+++ JS Basics/Objects and Array methods/Problem_6/index.js
@@ -1,4 +1,13 @@
 function group(arr, criteria) {
+  const groupedPeople = arr.reduce((groupPeople, person) => {
+    groupPeople[person[criteria]] = groupPeople[person[criteria]] || [];
+    groupPeople[person[criteria]].push(person);
+    return groupPeople;
+  }, {});
+  return groupedPeople;
+}
+
+/*
     const groupedPeople = {};
 
     for(const person of arr) {
@@ -11,16 +20,16 @@ function group(arr, criteria) {
         groupedPeople[groupKey].push(person);
     }
     return groupedPeople
-}
+*/
 
 let people = [
-    { firstname: 'Gosho', lastname: 'Petrov', age: 32 },
-    { firstname: 'Bay', lastname: 'Ivan', age: 81 },
-    { firstname: 'John', lastname: 'Doe', age: 42 },
-    { firstname: 'Pesho', lastname: 'Pesho', age: 22 },
-    { firstname: 'Asdf', lastname: 'Xyz', age: 81 },
-    { firstname: 'Gosho', lastname: 'Gosho', age: 22 }
+  { firstname: "Gosho", lastname: "Petrov", age: 32 },
+  { firstname: "Bay", lastname: "Ivan", age: 81 },
+  { firstname: "John", lastname: "Doe", age: 42 },
+  { firstname: "Pesho", lastname: "Pesho", age: 22 },
+  { firstname: "Asdf", lastname: "Xyz", age: 81 },
+  { firstname: "Gosho", lastname: "Gosho", age: 22 },
 ];
-  
+
 const groupByAge = group(people, "age");
 console.log(groupByAge);
