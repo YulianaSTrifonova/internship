a7e463b39b27a47fc45f1fe9281dd7014815f84e
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-03-12 16:43:02 +0200

    Fix Problem 2

--- JS Basics/Strings/Problem_2/index.js
+++ JS Basics/Strings/Problem_2/index.js
@@ -1,18 +1,28 @@
+const BRACKETS = {
+    OPEN: "(",
+    CLOSE: ")"
+}
+
+const RESULT = {
+    CORRECT: "Correct",
+    INCORRECT: "Incorrect"
+}
+
 function correctBrackets(arr) {
     let bracketsArr = [];
     const str = arr.toString();
 
     for(let i = 0; i < str.length; i++) {
-        if(str[i] === '(') {
+        if(str[i] === BRACKETS.OPEN) {
             bracketsArr.push(str[i]);
-        } else if(str[i] === ')') {
+        } else if(str[i] === BRACKETS.CLOSE) {
             if(bracketsArr.length === 0) {
-                return "Incorrect";
+                return RESULT.INCORRECT;
             }
             bracketsArr.pop();
         }
     }
-    return bracketsArr.length === 0 ? "Correct" : "Incorrect";
+    return bracketsArr.length === 0 ? RESULT.CORRECT : RESULT.INCORRECT;
 }
 
 correctBrackets([')(a+b))']);
