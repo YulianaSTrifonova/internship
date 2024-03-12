cf8c6220dd27a5b11cfde953275356a40bd732ce
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-03-12 17:30:46 +0200

    Fix Problem 7

--- JS Basics/Objects and Array methods/Problem_7/index.js
+++ JS Basics/Objects and Array methods/Problem_7/index.js
@@ -1,3 +1,30 @@
+const firstname = [
+  "Millie",
+  "Hailey",
+  "Miranda",
+  "Zoe",
+  "Molly",
+  "Diego",
+  "Isaac",
+  "Edward",
+  "Sam",
+  "Aiden",
+];
+const lastname = [
+  "Abbott",
+  "Abraham",
+  "Adams",
+  "Aldridge",
+  "Allen",
+  "Anderson",
+  "Armstrong",
+  "Banks",
+  "Barnett",
+  "Barrow",
+];
+const age = [19, 21, 23, 25, 26, 27, 29, 33, 37, 45];
+const gender = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
+
 function createPerson(firstname, lastname, age, gender) {
   return {
     firstname: firstname,
@@ -9,54 +36,12 @@ function createPerson(firstname, lastname, age, gender) {
 
 function generatePersons() {
   const people = [];
-  const firstnameFemale = [
-    "Ally",
-    "Lyla",
-    "Kylia",
-    "Daniela",
-    "Reily",
-    "Millie",
-    "Hailey",
-    "Miranda",
-    "Zoe",
-    "Molly",
-  ];
-  const firstnameMale = [
-    "Alex",
-    "Aaron",
-    "Austin",
-    "Bryce",
-    "Jacob",
-    "Diego",
-    "Isaac",
-    "Edward",
-    "Sam",
-    "Aiden",
-  ];
-  const lastname = [
-    "Abbott",
-    "Abraham",
-    "Adams",
-    "Aldridge",
-    "Allen",
-    "Anderson",
-    "Armstrong",
-    "Banks",
-    "Barnett",
-    "Barrow",
-  ];
-  const age = [19, 21, 23, 25, 26, 27, 29, 33, 37, 45];
-  const gender = ["female", "male"];
 
-  lastname.forEach((personLastname) => {
-    //let age = parseInt(Math.floor(Math.random() * 20) + Math.random() * 20);
-    let personAge = age[parseInt(Math.random() * 10)];
-    let personGender = gender[Math.random() < 0.5 ? 0 : 1];
-    if (personGender === "female") {
-      personFirstname = firstnameFemale[parseInt(Math.random() * 10)];
-    } else {
-      personFirstname = firstnameMale[parseInt(Math.random() * 10)];
-    }
+  firstname.forEach((personFirstname) => {
+    const index = firstname.indexOf(personFirstname);
+    const personLastname = lastname[index];
+    const personAge = age[index];
+    const personGender = gender[index];
 
     const person = createPerson(
       personFirstname,
