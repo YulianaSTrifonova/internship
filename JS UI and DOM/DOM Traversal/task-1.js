923c4082487c0ec56871a466b7a3ed6c837d5be0
Yuliana Trifonova <yuliana.s.trifonova@gmail.com>
2024-03-20 18:00:22 +0200

    Use querySelectorAll

--- JS UI and DOM/DOM Traversal/task-1.js
+++ JS UI and DOM/DOM Traversal/task-1.js
@@ -1,21 +1,24 @@
 function solve() {
-    let selectAside = document.querySelector("aside");
-    let nonSpecialAnchor = Array.from(selectAside.children).filter(
-        (child) => !child.classList.contains("special-anchor"));
-    nonSpecialAnchor.forEach((child) => console.log(child.innerText));
+  let selectAside = document.querySelector("aside");
 
-    console.log(" ");
-    let specialAnchor = document.querySelectorAll(".special-anchor");
-    specialAnchor.forEach((child) => console.log(child.innerText));
+  let nonSpecialAnchor = [...document.querySelectorAll("a")].filter(
+    (child) => selectAside.contains(child) && !child.classList.contains("special-anchor") 
+  );
+  nonSpecialAnchor.forEach((child) => console.log(child.innerText));
 
-    console.log(" ");
-    let redSpecialAnchor = document.querySelectorAll(".special-anchor[style*='background-color: red']");
-    redSpecialAnchor.forEach((child) => console.log(child.innerText));
+  console.log(" ");
+  let specialAnchor = document.querySelectorAll(".special-anchor");
+  specialAnchor.forEach((child) => console.log(child.innerText));
 
-    console.log(" ");
-    let selectMain = document.getElementById("main-element-id");
+  console.log(" ");
+  let redSpecialAnchor = document.querySelectorAll(
+    ".special-anchor[style*='background-color: red']"
+  );
+  redSpecialAnchor.forEach((child) => console.log(child.innerText));
 
-    let anchor = Array.from(selectMain.querySelectorAll("a.special-anchor")).filter(
-        (anchor) => anchor.style.backgroundColor !== "red");
-  anchor.forEach((anchor) => console.log(anchor.innerText));
+  console.log(" ");
+  let selectMain = document.getElementById("main-element-id");
+
+  let anchors = [...document.querySelectorAll("a.special-anchor")].filter((anchor) => selectMain.contains(anchor) && anchor.style.backgroundColor !== "red");
+  anchors.forEach((anchor) => console.log(anchor.innerText));
 }
